a5dcebaea5a060fde173e10443c419c78823a29a
Joona <zockedidockhd@gmail.com>
2022-02-10 19:19:31 +0100

    - splitted into diffrent files - added Routing - added Browsertem

---
+++ lib/browser_item.dart
@@ -0,0 +1,34 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class BrowserItem extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Icon icon;
+  final String text;
+  final double? fontSize;
+  final Color? textColor;
+
+  const BrowserItem({
+    Key? key,
+    required this.onPressed,
+    required this.icon,
+    required this.text,
+    this.fontSize,
+    this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          icon: icon,
+          onPressed: () => onPressed(),
+        ),
+        Text(
+          text,
+          style: TextStyle(fontSize: fontSize, color: textColor),
+        ),
+      ],
+    );
+  }
+}
